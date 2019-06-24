ffb7b2fd1c46df99c03bce7c482415b6db118aca
punya <punyachapagain@gmail.com>
2019-06-24 11:45:20 +1000

    add main component

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Main from './component/main.jsx'
 import './App.css';
 
 
 class App extends Component {
-  state = {
-    name: ""
-  }
 
-  componentDidMount() {
-    fetch("/api")
-      .then(resp => resp.json())
-      .then(data => {
-
-        this.setState({ name: data.name })
-      })
-
-  }
   render() {
 
     return (
 
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <Main />
 
-
-        </header>
-        <h1>{this.state.name}</h1>
-        <h1>The Name of Some Fruits</h1>
-
-
-
-        <div>
-
-        </div>
       </div>
 
     );
